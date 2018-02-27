618585b65d629ecfc35aef1d44e34f94252a6e45
Administrateur <Administrateur@10.5.33.50>
2018-02-27 09:09:00 +0100

    Correct conflict.

--- dao.php
+++ dao.php
@@ -1,55 +1,55 @@
 <?php
+
 require_once './bdd/mySql.inc.php';
 require_once './bdd/connectionBase.php';
 
-function getAllPokemon(){
+function getAllPokemon() {
 
-        $resultat = pokedb()->query("SELECT `pokemonDexId`, `pokemonName`  FROM `pokemon`
+    $resultat = pokedb()->query("SELECT `pokemonDexId`, `pokemonName`  FROM `pokemon`
 ")->fetchAll(PDO::FETCH_ASSOC);
-        $nomColl = array("Id","Nom");
+    $nomColl = array("Id", "Nom");
 
-        echo "<table>";
-        foreach ($nomColl AS $key => $valeur){
-            echo "<th>$valeur</th>";
+    echo "<table>";
+    foreach ($nomColl AS $key => $valeur) {
+        echo "<th>$valeur</th>";
+    }
+    foreach ($resultat AS $key => $valeur) {
+        echo "<tr>";
+        foreach ($valeur AS $key => $valeur2) {
+            echo "<td>$valeur2</td>";
         }
-        foreach ($resultat AS $key => $valeur){
-            echo "<tr>";
-            foreach ($valeur AS $key => $valeur2){
-                echo "<td>$valeur2</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";
-
+        echo "</tr>";
+    }
+    echo "</table>";
 }
 
-function getType($id){
+function getType($id) {
 
-  $idTypes = pokedb()->query("SELECT `typeId` FROM `istype` WHERE `pokemonId` = $id ;");
-  echo $idTypes;
-    if($id)
-  foreach ($idTypes as $key => $value) {
-      $idTypes += pokedb()->query("SELECT `typeName` FROM `type` WHERE `typeId` =$value ") .
-    "SELECT distinct t.typeName FROM istype as i, type as t, pokemon as p WHERE t.typeId = i.typeId AND i.pokemonId = '1'";
+    $idTypes = pokedb()->query("SELECT `typeId` FROM `istype` WHERE `pokemonId` = $id ;");
+    echo $idTypes;
+    if ($id)
+        foreach ($idTypes as $key => $value) {
+            $idTypes += pokedb()->query("SELECT `typeName` FROM `type` WHERE `typeId` =$value ") .
+                    "SELECT distinct t.typeName FROM istype as i, type as t, pokemon as p WHERE t.typeId = i.typeId AND i.pokemonId = '1'";
+        }
 
-  }
-  
-function getAllAttack(){
-    $resultat = pokedb()->query("SELECT `moveName`, `movePower`, `moveAccuracy` FROM `move`
+    function getAllAttack() {
+        $resultat = pokedb()->query("SELECT `moveName`, `movePower`, `moveAccuracy` FROM `move`
 ")->fetchAll(PDO::FETCH_ASSOC);
         $nomColl = array("Attaque", "Puissance", "Précision");
 
         echo "<table>";
-        foreach ($nomColl AS $key => $valeur){
+        foreach ($nomColl AS $key => $valeur) {
             echo "<th>$valeur</th>";
         }
-        foreach ($resultat AS $key => $valeur){
+        foreach ($resultat AS $key => $valeur) {
             echo "<tr>";
-            foreach ($valeur AS $key => $valeur2){
+            foreach ($valeur AS $key => $valeur2) {
                 echo "<td>$valeur2</td>";
             }
             echo "</tr>";
         }
         echo "</table>";
+    }
+
 }
-      ?>
