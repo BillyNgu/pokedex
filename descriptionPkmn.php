a0e28731a98a013ec99b147d0ef3cc3d695812b3
loicdub <loic.dbsmr@eduge.ch>
2018-02-27 09:40:09 +0100

    add sprite folder and files

--- descriptionPkmn.php
+++ descriptionPkmn.php
@@ -8,11 +8,6 @@ require_once './bdd/mySql.inc.php';
 require_once './dao.php';
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
