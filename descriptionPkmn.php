0f61fb3f34b03bbfe9e331ff4759cc3ce1b4d6bb
loicdub <loic.dbsmr@eduge.ch>
2018-02-27 09:29:52 +0100

    add pokemon and attack description page

---
+++ descriptionPkmn.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Authors : Dubas Loïc, Pighini Lucas, Nguyen Billy
+ * Description : Description of the selected Pokemon
+ * Date : 27.02.18
+ */
+require_once './bdd/mySql.inc.php';
+require_once './dao.php';
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
