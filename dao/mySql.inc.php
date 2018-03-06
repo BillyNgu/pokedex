c65f4134f7f45fe91fd1f928579adc0c6e1ff05d
loicdub <loic.dbsmr@eduge.ch>
2018-03-06 10:44:40 +0100

    connection settings modified

--- dao/mySql.inc.php
+++ dao/mySql.inc.php
@@ -1,5 +1,13 @@
 <?php
+// LOCAL
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'pokedexdb');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
+
+// MOUETTE
+//define('DB_HOST', '10.5.33.20');
+//define('DB_PORT', ':3306');
+//define('DB_NAME', 'pokedexdb');
+//define('DB_USER', 'pokedexAdm');
+//define('DB_PASSWORD', 'Super');
