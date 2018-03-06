f00e22d505665298dfb4c72d72c21f32595d00f4
Administrateur <Administrateur@10.5.33.50>
2018-03-06 10:20:54 +0100

    Changing title.

--- types.php
+++ types.php
@@ -10,7 +10,7 @@ require_once './dao/dao.php';
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Pokedex</title>
+        <title>Pokédex</title>
         <link rel="stylesheet" href="css/bootstrap.css">
     </head>
     <body class="body">
