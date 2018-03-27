8797819faf5ac6b9e488839976eab5e827f1d77b
Administrateur <Administrateur@10.5.33.50>
2018-03-27 08:29:06 +0200

    Correct description code

--- descriptionPkmn.php
+++ descriptionPkmn.php
@@ -35,20 +35,19 @@ $pkmtype = getPokemonType($result['pokemonId']);
             <div class="card-group">
                 <div class="card col-4" style="width: 18rem;">
                     <div class="card-body">
-                        <img class="card-img" src="data:image/jpeg;base64, <?php echo base64_encode($result['pokemonImg']); ?>" />
+                        <img class="card-img" src="data:image/jpeg;base64, <?= base64_encode($result['pokemonImg']); ?>" />
                     </div>
                 </div>
                 <div class="card w-75" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $result['pokemonName']; ?></h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Numéro national : <?php echo $result['pokemonId']; ?></h6>
-                        <h6> Type : <?php
-                            foreach ($pkmtype as $key => $value) {
-                                echo '<img src="data:image/jpeg;base64,' . base64_encode($value['typeName']) . '">';
-                            }
-                            ?> </h6>
-                        <h5>Description : </h5><?php echo $result['pokemonDescription']; ?>
-                        
+                        <h5 class="card-title"><?= $result['pokemonName']; ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Numéro national : <?= $result['pokemonId']; ?></h6>
+                        <h6> Type : <?php foreach ($pkmtype as $key => $value): ?> 
+                                <img src="data:image/jpeg;base64,<?= base64_encode($value['typeName']) ?>">
+                            <?php endforeach; ?>
+                        </h6>
+                        <h5>Description : </h5><?= $result['pokemonDescription']; ?>
+
                     </div>
                 </div>
             </div>
