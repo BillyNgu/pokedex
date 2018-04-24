1affa2bb0c81c463ac6362e2bf26203e50d4e68f
Administrateur <Administrateur@10.5.33.50>
2018-04-24 07:57:01 +0200

    Modify navbar

--- register.php
+++ register.php
@@ -54,47 +54,7 @@ if (!empty($_SESSION['userNickname'])) {
     </head>
     <body class="body">
         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="index.php">Pokédex</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">Index</a>        
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="attacks.php">Liste des attaques</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="types.php">Liste des types</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tableau des faiblesses</a>
-                        </li>
-                    </ul>
-                </div>
-                <?php if (!empty($nickname)): ?>
-                    <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link active">Bienvenue <?= $nickname; ?></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active"></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Déconnexion</a>
-                        </li>
-                    </ul>
-                <?php else: ?>
-                    <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link" href="connection.php">Connexion</a>
-                        </li>
-                    </ul>
-                <?php endif; ?>
-            </nav>
+            <?php require_once './navbar.php'; ?>
             <form action="register.php" method="post">
                 <h3>Inscription</h3>
                 <div class="form-group">
